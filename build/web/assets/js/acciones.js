cd0a033e67c7c2b6eea4a459dd5bd2b61686d86c
Aura vargas <aurayorleny@gmail.com>
2020-02-17 14:59:05 -0500

    calendario con crud de evento funcional

---
+++ build/web/assets/js/acciones.js
@@ -0,0 +1,22 @@
+
+function consultar(Titulo,Descripcion,estado,fecha,id){
+    ide = id;
+        document.getElementById("Titulo").innerHTML =Titulo;
+        $("#ventana1").modal("hide");
+        document.getElementById('descripcion').innerHTML=Descripcion;
+        document.getElementById('fecha').innerHTML="Fecha: "+fecha;
+        document.getElementById('estado').innerHTML="Estado: "+estado;
+        document.getElementById('editar').href="ControladorAgenda?accion=editar&codigo="+id+"&desde=1";
+      };
+function elim(){
+          var con =confirm('¿Esta seguro de eliminar este evento?');
+          if(con){
+            location.href = 'ControladorAgenda?accion=elim&txtcodigo='+ide+'&desde=1';
+          }
+}; 
+function eliminar(id){
+          var con =confirm('¿Esta seguro de eliminar este evento?');
+          if(con){
+            location.href = 'ControladorAgenda?accion=eliminar&txtcodigo='+id;
+          }
+};
